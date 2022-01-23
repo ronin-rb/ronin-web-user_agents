cc1f84a451fad32c47b199c2ffd99ec8f96e0d4f
Postmodern <postmodern.mod3@gmail.com>
2022-01-22 22:32:24 -0800

    Changed project name to ronin-web-user_agents.

--- lib/ronin/web/user_agents.rb
+++ lib/ronin/web/user_agents.rb
@@ -1,23 +1,22 @@
 #
-# Ronin Web - A Ruby library for Ronin that provides support for web
-# scraping and spidering functionality.
+# ronin-web-user_agents - Yet another User-Agent randomiser library.
 #
 # Copyright (c) 2006-2021 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
-# This file is part of ronin-web.
+# This file is part of ronin-web-user_agents.
 #
-# Ronin is free software: you can redistribute it and/or modify
+# ronin-web-user_agents is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ronin is distributed in the hope that it will be useful,
+# ronin-web-user_agents is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ronin.  If not, see <https://www.gnu.org/licenses/>.
+# along with ronin-web-user_agents.  If not, see <https://www.gnu.org/licenses/>
 #
 
 require 'ronin/web/config'
