12e8d1c4f29a450e72e1026139dc7e6109485d7d
Postmodern <postmodern.mod3@gmail.com>
2022-02-13 12:45:27 -0800

    Added `Ronin::Web::UserAgents::OS::Windows`.

---
+++ lib/ronin/web/user_agents/os/windows.rb
@@ -0,0 +1,51 @@
+#
+# ronin-web-user_agents - Yet another User-Agent randomiser library.
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-web-user_agents.
+#
+# ronin-web-user_agents is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-web-user_agents is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-web-user_agents.  If not, see <https://www.gnu.org/licenses/>
+#
+
+module Ronin
+  module Web
+    module UserAgents
+      module OS
+        #
+        # @api private
+        #
+        module Windows
+          # Common Windows versions.
+          VERSIONS = {
+            xp:    '5.2',
+            vista: '6.0',
+            7   => '6.1',
+            8   => '6.2',
+            8.1 => '6.3',
+            10  => '10.0'
+          }
+
+          # Architectures supported by Windows.
+          ARCHES = {
+            wow64: 'WOW64',
+            win64: 'Win64; x64',
+
+            nil => nil
+          }
+        end
+      end
+    end
+  end
+end
---
+++ spec/os/windows_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+require 'ronin/web/user_agents/os/windows'
+
+describe Ronin::Web::UserAgents::OS::Windows do
+  describe "VERSIONS" do
+    subject { described_class::VERSIONS }
+
+    it "must map :xp to '5.2'" do
+      expect(subject[:xp]).to eq('5.2')
+    end
+
+    it "must map :vista to '6.0'" do
+      expect(subject[:vista]).to eq('6.0')
+    end
+
+    it "must map 7 to '6.1'" do
+      expect(subject[7]).to eq('6.1')
+    end
+
+    it "must map 8 to '6.2'" do
+      expect(subject[8]).to eq('6.2')
+    end
+
+    it "must map 8.1 to '6.3'" do
+      expect(subject[8.1]).to eq('6.3')
+    end
+
+    it "must map 10 to '10.0'" do
+      expect(subject[10]).to eq('10.0')
+    end
+  end
+
+  describe "ARCHES" do
+    subject { described_class::ARCHES }
+
+    it "must map :wow64 to 'WOW64'" do
+      expect(subject[:wow64]).to eq('WOW64')
+    end
+
+    it "must map :win64 to 'Win64; x64'" do
+      expect(subject[:win64]).to eq('Win64; x64')
+    end
+
+    it "must map nil to nil" do
+      expect(subject[nil]).to be(nil)
+    end
+  end
+end
