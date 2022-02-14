a559f74e23e6c931e6fed041f98c4ca52d379946
Postmodern <postmodern.mod3@gmail.com>
2022-02-13 16:04:12 -0800

    Forgot to commit `ronin/web/user_agents/data_dir`.

---
+++ lib/ronin/web/user_agents/data_dir.rb
@@ -0,0 +1,31 @@
+#
+# ronin-web-user_agents - Yet another User-Agent randomiser library.
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-web-user_agents.
+#
+# ronin-web-user_agents is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-web-user_agents is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-web-user_agents.  If not, see <https://www.gnu.org/licenses/>
+#
+
+module Ronin
+  module Web
+    module UserAgents
+      # Path to the `data/` directory.
+      #
+      # @api private
+      DATA_DIR = File.expand_path('../../../../data',__dir__)
+    end
+  end
+end
